74402860cc25b595baaef4a98bce381e9a95674a
Aman Thakur <106446742+aman7thakurr@users.noreply.github.com>
2024-06-24 07:21:27 +0000

    update

--- app.js
+++ app.js
@@ -1,3 +1,37 @@
-const backward = document.querySelector('.backward')
-const forward = document.querySelector('.forward')
-const play = document.querySelector('play')
+const aud = document.getElementById('aud')
+var audio = new Audio("./assets/powfu-death-bed.mp3")
+const btn = document.querySelector('.botón')
+const volumecontroller = document.getElementById("volumecontroller")
+
+function playPause(){
+    if(btn.className =='botón active'){  
+
+  
+    audio.play();
+}
+else{
+
+
+    audio.pause();
+}}
+volumecontroller.addEventListener('input',()=>{
+    HTMLMediaElement.volume = volumecontroller.value/100
+})
+console.log(volumecontroller.value)
+const player = document.querySelector('.fake-player');
+
+function clickHandler () { 
+    const buttons = Array.from(this.children);
+    buttons.forEach(button => button.classList.toggle('hidden'))
+    if(buttons.classList.toggle('hidden')){
+        pauseAud()
+    }
+};
+ 
+player.addEventListener('click', clickHandler);
+
+function playAud(s){
+    var song = new Audio(s)
+    console.log(song);
+song.play()
+}
